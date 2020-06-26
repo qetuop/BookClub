aaf7bef4327c95f7a4013569d57f5723f7c72642
qetuop <bdonson@yahoo.com>
2020-06-26 13:08:55 -0400

    startings of the RESTful service, can respond to basic get/puts

--- src/main/java/com/qetuop/bookclub/model/Book.java
+++ src/main/java/com/qetuop/bookclub/model/Book.java
@@ -42,8 +42,16 @@ public class Book {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getAuthor() {
         return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
     
 }
--- src/main/java/com/qetuop/bookclub/repository/BookRepository.java
+++ src/main/java/com/qetuop/bookclub/repository/BookRepository.java
@@ -4,11 +4,20 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import com.qetuop.bookclub.model.Book;
 
-public interface BookRepository extends CrudRepository<Book, Long> {
+//public interface BookRepository extends CrudRepository<Book, Long> {
 
-  List<Book> findByAuthor(String author);
+// direct Spring MVC to create RESTful endpoints at '/books'. the defalut value is 'books' and this not needed
+@RepositoryRestResource(collectionResourceRel = "books", path = "books") 
+public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
+
+  //List<Book> findByAuthor(String author);
+  List<Book> findByAuthor(@Param("author") String author);
 
   Book findById(long id);
 }
