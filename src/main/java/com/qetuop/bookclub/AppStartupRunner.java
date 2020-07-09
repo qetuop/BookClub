69206b5edf6ea2c333378fbeefc95cfb947f753a
qetuop <bdonson@yahoo.com>
2020-07-09 14:19:56 -0400

    some more parsing logic.  temp hacks to exclude some and handle non clean dir names, should parse entire audiobooks now

--- src/main/java/com/qetuop/bookclub/AppStartupRunner.java
+++ src/main/java/com/qetuop/bookclub/AppStartupRunner.java
@@ -1,5 +1,6 @@
 package com.qetuop.bookclub;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -39,7 +40,7 @@ public class AppStartupRunner implements ApplicationRunner {
 	@Autowired
 	public StorageService storageService;
 	public BookRepository repository;
-	
+
 	public FileList fileList = new FileList();
 
 	public AppStartupRunner(StorageService storageService, BookRepository repository) {
@@ -50,87 +51,119 @@ public class AppStartupRunner implements ApplicationRunner {
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		LOG.info("AppStartupRunner::print files");
-		
-		String rootDir = "/home/brian/Projects/testdir/audio books/";       
-        //rootDir = "/media/NAS/audiobooks";
-		
-		List<Path> fileWithName = fileList.printFiles(rootDir);
-		
-		HashMap<Integer,Long> bookMap = new HashMap<Integer,Long>();
-		
-		for (Path filePath : fileWithName) {
-            System.out.println("\n------New file\n" + filePath);            
-            
-            String fullPathString = filePath.toString().strip();
-            
-            String tmpString = fullPathString.replace(rootDir, "");
-            String pattern = Pattern.quote(System.getProperty("file.separator"));
-            String[] splitList = tmpString.toString().split(pattern);
-            
-            System.out.println("\t\t*" + StringUtils.join(splitList, "|") +"*");
-            
-            // standalone book
-            // |Brian Herbert|Paul of Dune|Paul of Dune.jpg
-            
-            // series 
-            // |Frank Herbert|Dune|Dune - 1|Dune.jpg
-            
-            String author = null;
-            String title = null;
-            String path = null; // rootDir + Author + series + title  or fullPath - last element?
-            String cover = null; // save largest image == replace if necessary
-            Byte[] image = null;
-            String seriesName = null;
-            int seriesNumber = 0;
-            
-            author = splitList[0];
 
-            if ( splitList.length == 3 ) {
-            	title = splitList[1];
-            	path = rootDir + "/" + author + "/" + title;
-            }
-            else if (splitList.length == 4 ) {
-            	
-            	seriesName = splitList[1];
-            	String titleFull = splitList[2];
-            	
-            	String[] splitTitle = titleFull.toString().split("-");
-            	System.out.println("\t\t" + StringUtils.join(splitTitle, "|"));
-            	title = splitTitle[0].strip();
-            	seriesNumber = Integer.parseInt(splitTitle[1].strip());
-            	System.out.println("\t\t" + seriesName + ":" + seriesNumber);
-            	
-            	
-            	path = rootDir + "/" + author + "/" + seriesName + " - " + seriesNumber + "/" + title;
-            }
-            else {
-            	System.out.println("somthings wrong");
-            }
-            
-            cover = fullPathString;
-            
-            
-            final File file = new File(fullPathString);
+		// TODO: figure out if i should include trailing slash or not, it affects the split below, just be consistent
+		String rootDir = "/home/brian/Projects/testdir/audio books/";       
+		//rootDir = "/media/NAS/audiobooks/";
+
+		List<Path> fileWithName = fileList.printFiles(rootDir);
+
+		// keep a map of hash(author/series/title):book id for quicker lookup during operations below - worth it?
+		// link to the book object itself then add all at once to repo?  TODO: do that
+		HashMap<Integer,Long> bookMap = new HashMap<Integer,Long>();
+
+		List<String> ignoreList = new ArrayList<String>(Arrays.asList("#recycle", "_temp", "_organize", "Warhammer"));
+
+
+		for (Path filePath : fileWithName) {
+			System.out.println("\n------New file: " + filePath);            
+
+			String fullPathString = filePath.toString().strip();
+
+			String tmpString = fullPathString.replace(rootDir, "");
+			String pattern = Pattern.quote(System.getProperty("file.separator"));
+			String[] splitList = tmpString.toString().split(pattern);
+
+			System.out.println("\t\t*" + StringUtils.join(splitList, "|") +"*");
+
+			// standalone book
+			// |Brian Herbert|Paul of Dune|Paul of Dune.jpg
+
+			// series 
+			// |Frank Herbert|Dune|Dune - 1|Dune.jpg
+
+			String author = null;
+			String title = null;
+			String path = null; // rootDir + Author + series + title  or fullPath - last element?
+			String cover = null; // save largest image == replace if necessary
+			Byte[] image = null;
+			String seriesName = null;
+			int seriesNumber = 0;
+
+			author = splitList[0];
+
+			System.out.println("AUTHOR: " + author + "," + splitList[1]);
+			// HACK check - ignore these dirs TODO: fix this
+			if ( ignoreList.contains(author)) {
+				continue;
+			}
+
+			// single book
+			if ( splitList.length == 3 ) {
+				title = splitList[1];
+				path = rootDir + "/" + author + "/" + title;
+			}
+			// series
+			else if (splitList.length == 4 ) {
+
+				seriesName = splitList[1];
+				String titleFull = splitList[2];
+
+
+				// TODO: can i just split on spaces, if there is a number last wether preceded by a - or not, the re join everyting not a -
+				if ( titleFull.matches(".*-.*") ) {
+
+					// TODO: once i've properly named dir/files - may need to handle anyways
+					String[] splitTitle = titleFull.toString().split("-");
+
+
+					//System.out.println("\t\t" + StringUtils.join(splitTitle, "|"));
+					title = splitTitle[0].strip();
+					seriesNumber = Integer.parseInt(splitTitle[1].strip());
+
+				}
+				else if ( titleFull.matches(".*\\d.*") ) {
+					String[] splitTitle = titleFull.toString().split(" ");
+					String seriesNumberStr = splitTitle[splitTitle.length-1];
+					seriesNumber = Integer.parseInt(seriesNumberStr);
+					//System.out.println("SERIES NUM: " + seriesNumber);
+					title = titleFull.substring(0, titleFull.indexOf(seriesNumberStr)-1);					
+				}
+
+				//System.out.println("\t\t" + seriesName + ":" + seriesNumber);
+
+
+				path = rootDir + "/" + author + "/" + seriesName + " - " + seriesNumber + "/" + title;
+			}
+			else {
+				System.out.println("somthings wrong");
+			}
+
+			cover = fullPathString;
+
+
+			final File file = new File(fullPathString);
 
 			FileInputStream input = null;
 			try {
 				input = new FileInputStream(file);
 				image = ArrayUtils.toObject(IOUtils.toByteArray(input));
 
-				
+
 				String[] strings = {author, seriesName, title};
 				int hashCode = Arrays.hashCode(strings);
-				System.out.println(hashCode);		
-				
+				//System.out.println(hashCode);		
+
 				Book book = null;
-				
+
 				// set the cover to the largest image found in the dir.  TODO: is this the best thing to do?
 				if ( bookMap.containsKey(hashCode) ) {
 
 					Optional<Book> found = repository.findById(bookMap.get(hashCode));
+
 					if (found.isPresent()) {
 						book = found.get();
-						
+
 						if ( book.getImage().length < image.length )  {
 							book.setImage(image);						
 						}
@@ -140,10 +173,10 @@ public class AppStartupRunner implements ApplicationRunner {
 					// Book(String title, String author, String path, String cover, Byte[] image, String seriesName, int seriesNumber)
 					book = new Book(title, author, path, cover, image, seriesName, seriesNumber);				
 				}
-				
+
 				book = repository.save(book);	
 				bookMap.put(hashCode, book.getId());
-				
+
 
 			} catch (final FileNotFoundException e) {
 				// TODO Auto-generated catch block
@@ -153,12 +186,12 @@ public class AppStartupRunner implements ApplicationRunner {
 				e.printStackTrace();
 			}
 
-            //File file = new File(fullPathString);
-            //String simpleFileName = file.getName();
-            //System.out.println("\t"+simpleFileName);
-        }
+			//File file = new File(fullPathString);
+			//String simpleFileName = file.getName();
+			//System.out.println("\t"+simpleFileName);
+		}
 	}
-	
+
 	/*
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
@@ -187,13 +220,13 @@ public class AppStartupRunner implements ApplicationRunner {
 				Byte[] image = ArrayUtils.toObject(IOUtils.toByteArray(input));
 
 				final Book book = new Book(array[0], array[1], array[2], array[3], image);
-				
+
 
 				repository.save(book);
-				
+
 				// MultipartFile multipartFile = new MockMultipartFile("file",file.getName(),
 				// "text/plain", IOUtils.toByteArray(input));
-				
+
 
 			} catch (final FileNotFoundException e) {
 				// TODO Auto-generated catch block
@@ -204,5 +237,5 @@ public class AppStartupRunner implements ApplicationRunner {
 			}
 		});
 	} // run
-	*/
+	 */
 }
